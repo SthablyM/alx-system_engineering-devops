24ef430d303005314eaab6c7095985863d8708ec
SthablyM <mathapelofentse@icloud.com>
2024-05-03 17:58:21 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,10 +2,15 @@
 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3,'
+  provider => 'pip3',
 }
 
 package { 'werkzeug':
   ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+package { 'Python':
+  ensure   => '3.8.10',
   provider => 'pip3',
 }
