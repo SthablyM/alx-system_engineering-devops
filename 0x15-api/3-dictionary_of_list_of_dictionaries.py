dbf3393397c210328cd461dc7112daf3622667e0
SthablyM <mathapelofentse@icloud.com>
2024-06-05 00:32:16 -0700

    3-dictionary_of_list_of_dictionaries.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,22 +3,27 @@
 import json
 import requests
 
+
 def fetch_user_data():
     url = 'https://jsonplaceholder.typicode.com/'
     users = requests.get(url + 'users').json()
-    data_to_export = {}
+    data_export = {}
     for user in users:
         user_id = user['id']
         user_url = url + f'todos?userld ={user_id}'
-        todo_list = request.get(user_url).json()
-        data_to_export[user_id] = [
-                {'task': todo.get('title', 'completed': todo.get('completed'), 'username': user.get('username')),}
-                for t in todo_list
+        todos = requests.get(user_url).json()
+        data_export[user_id] = [
+                {
+                    'task': todo.get('title'),
+                    'completed': todo.get('completed'),
+                    'username': user.get('username'),
+                }
+                for todo in todos
                 ]
-    return data_to_export
+    return data_export
 
 
 if __name__ == "__main__":
-    data_to_export = fetch_user_data()
+    data_export = fetch_user_data()
     with open('todo_all_employees.json', 'w') as jsonfile:
-        json.dump(data_to_export,jsonlife, indent=4)
+        json.dump(data_export, jsonfile, indent=4)
