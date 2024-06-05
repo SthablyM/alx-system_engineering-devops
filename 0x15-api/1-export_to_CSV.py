d6dcb35ab4d10948489d30c54576c4fe58e88d4c
SthablyM <mathapelofentse@icloud.com>
2024-06-05 00:07:18 -0700

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,24 +1,35 @@
 #!/usr/bin/python3
-"""script to export data in the CSV format."""
+
+"""
+Python script that exports data in the CSV format
+"""
+
+from requests import get
+from sys import argv
 import csv
-import requests
-import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = 'https://jsonplaceholder.typicode.com/'
-    user_response = requests.get(url + f'users/{user_id}')
-    user = user_response.json()
-    username = user.get('username')
-    todos_response = requests.get(url + 'todos', params={'userId': user_id})
-    todos = todos_response.json()
-    with open(f'{user_id}.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        writer.writerow(
-                ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
-        for t in todos:
-            writer.writerow(
-                    [user_id, username, t.get('completed'), t.get('title')])
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    data = response.json()
 
-    print(f"Number of tasks in CSV: {'OK' if len(todos) == \
-    len(todos) else 'Incorrect'}")
+    row = []
+    responses = get('https://jsonplaceholder.typicode.com/users')
+    datat = responses.json()
+
+    for t in datat:
+        if t['id'] == int(argv[1]):
+            employee = t['username']
+
+    with open(argv[1] + '.csv', 'w', newline='') as file:
+        write = csv.writer(file, quoting=csv.QUOTE_ALL)
+
+        for t in data:
+
+            row = []
+            if t['userId'] == int(argv[1]):
+                row.append(t['userId'])
+                row.append(employee)
+                row.append(t['completed'])
+                row.append(t['title'])
+
+                write.writerow(row)
