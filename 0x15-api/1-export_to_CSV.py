4f6b3b3aa3f2fcb761d698a0d245bf76b502087e
SthablyM <mathapelofentse@icloud.com>
2024-06-04 23:59:50 -0700

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,11 +7,18 @@ import sys
 if __name__ == "__main__":
     user_id = sys.argv[1]
     url = 'https://jsonplaceholder.typicode.com/'
-    user = requests.get(url + 'users/{}'.format(user_id)).json()
+    user_response = requests.get(url + f'users/{user_id}')
+    user = user_response.json()
     username = user.get('username')
-    todos = requests.get(url + 'todos', params={'userld': user_id}).json()
-    with open('{}.csv'.format(user_id), 'w', newline='')as csvfile:
+    todos_response = requests.get(url + 'todos', params={'userId': user_id})
+    todos = todos_response.json()
+    with open(f'{user_id}.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get('completed'), t.get('title')]
-        ) for t in todos]
+        writer.writerow(
+                ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        for t in todos:
+            writer.writerow(
+                    [user_id, username, t.get('completed'), t.get('title')])
+
+    print(f"Number of tasks in CSV: {'OK' if len(todos) == \
+    len(todos) else 'Incorrect'}")
