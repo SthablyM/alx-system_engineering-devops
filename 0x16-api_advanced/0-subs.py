acc8061300d00ead846a6918a2ca5489117aff11
SthablyM <mathapelofentse@icloud.com>
2024-06-18 04:06:46 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,10 +5,10 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (compatible; MyRedditBot/0.1)'} 
+    headers = {'User-Agent': 'Mozilla/5.0 (My_user-Agent)'} 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 400:
+        if response.status_code == 200:
             data = response.json()
             return data['data']['subscribers']
         else:
