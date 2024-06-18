62cf55e043701b2d1462a0b4bde4f21845d78c6b
SthablyM <mathapelofentse@icloud.com>
2024-06-18 04:11:58 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-""" function that queries the Reddit  API"""
+"""
+function that queries the Reddit API
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0 (My_user-Agent)'} 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
+    url = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
+
+    if url.status_code == 200:
+        return url.json().get("data").get("subscribers")
+    else:
         return 0
