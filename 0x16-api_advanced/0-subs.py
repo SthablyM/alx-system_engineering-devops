93ef8519b235e1a287f24a21ec163069c31a3d8a
SthablyM <mathapelofentse@icloud.com>
2024-08-13 03:35:53 -0700

    0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+ function that queries the Reddit API and returns the number of
+ subscribers
+"""
+import requests
+
+
+
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns the number of total subscribers"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if (not response.ok):
+        return 0
+    sub_acout = response.json().get("data")
+    return sub_acount.get("subscribers")
