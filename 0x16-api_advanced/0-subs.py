d9a0fd4c83ec5778934e84affb48b7239aa69b3f
SthablyM <mathapelofentse@icloud.com>
2024-08-13 05:53:12 -0700

    sub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,13 +9,14 @@ import requests
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of total subscribers"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 403:
-        return 0
-    data = response.json().get("data")
-    if data:
-        return data.get("subscribers", 0)
-    else:
-        return 0
+    headers = {'User-Agent': 'My-app/0.0.1'}
 
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" 
+recursive function that queries the Reddit API and returns a
+list containing
+"""
+import requests
+
+def recurse(subreddit, hot_list=None, after=None):
+    """Recursively retrieves hot articles from a subreddit."""
+    if hot_list is None:
+        hot_list = []
+
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    headers = {
+        'User-Agent': 'MyRedditApp/1.0 (http://example.com;mathapelo"gmail.com)'
+    }
+    
+    params = {'after': after} if after else {}
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if response.status_code == 404:
+        return
+    elif response.status_code != 200:
+        print("Error: Unable to fetch data.")
+        return None
+
+    try:
+        data = response.json().get('data', {})
+        children = data.get('children', [])
+
+        for child in children:
+            hot_list.append(child['data']['title'])
+
+        after = data.get('after')
+        if after:
+            return recurse(subreddit, hot_list, after)
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+    return hot_list
